fdf1efd451c15cab3ed1b71c9d787a60beaea694
Un1tace <kaiafful2008@gmail.com>
2025-07-02 16:38:37 +0100

    Fragment and vertex shaders added

--- CSCanbulatEngine/CSCanbulatEngine/obj/Debug/net9.0/CSCanbulatEngine.AssemblyInfo.cs
+++ CSCanbulatEngine/CSCanbulatEngine/obj/Debug/net9.0/CSCanbulatEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSCanbulatEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f85aed69dab028df833317f0bf95c499789af5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450719446fa4e63dccd943419526004fb6faf056")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSCanbulatEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSCanbulatEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
