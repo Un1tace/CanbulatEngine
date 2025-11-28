dac44c35cb78619fc1950aca109a0ff9ce7d5b95
Un1tace <kaiafful2008@gmail.com>
2025-11-28 17:51:52 +0000

    Template now fixed

--- CSCanbulatEngine/CSCanbulatEngine/obj/Debug/net9.0/CSCanbulatEngine.AssemblyInfo.cs
+++ CSCanbulatEngine/CSCanbulatEngine/obj/Debug/net9.0/CSCanbulatEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSCanbulatEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df04c5a5ba07b2d528158c33f69716bd7c2e1220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad241efce8931ddf6c074ddb980985ea0284ec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSCanbulatEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSCanbulatEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
